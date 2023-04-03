077df87603b54f7f84466ac98f43b36b9effd633
Petkevich <lpblackmesa@gmail.com>
2023-04-03 20:57:09 +0300

    Add partition lesson 14

---
+++ src/lesson14/homework1/Homework1.java
@@ -0,0 +1,17 @@
+package lesson14.homework1;
+
+public class Homework1 {
+    public static void main(String[] args) throws InterruptedException {
+
+        MyThread thread1 = new MyThread("1");
+        MyThread thread2 = new MyThread("2");
+        MyThread thread3 = new MyThread("3");
+
+        thread3.start();
+        thread3.join();
+        thread2.start();
+        thread2.join();
+        thread1.start();
+        thread1.join();
+    }
+}
